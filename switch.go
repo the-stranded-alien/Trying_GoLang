df642aae694e92c64466063ee7004d79e0a969e6
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-11-14 19:12:49 +0530

    switch statements

---
+++ switch.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	switch {
+	case false:
+		fmt.Println("this should not print")
+	case (2 == 4):
+		fmt.Println("this should not print2")
+	case (3 == 3):
+		fmt.Println("prints")
+	case (4 == 4):
+		fmt.Println("also true does it prints")
+	}
+
+	switch {
+	case false:
+		fmt.Println("this should not print")
+	case (2 == 4):
+		fmt.Println("this should not print2")
+	case (3 == 3):
+		fmt.Println("prints")
+		fallthrough
+	case (4 == 4):
+		fmt.Println("also true does it prints")
+	default:
+		fmt.Println("Default")
+	}
+}
