a909370a5c1796505d667367ba387e801d8247ea
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-11-17 14:55:10 +0530

    callbacks

---
+++ callbacks.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	ii := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
+	sum := add(ii...)
+	fmt.Println(sum)
+
+	ie := even(add, ii...)
+	fmt.Println(ie)
+}
+
+func add(xi ...int) int {
+	total := 0
+	for _, v := range xi {
+		total += v
+	}
+	return total
+}
+
+func even(f func(xi ...int) int, vi ...int) int {
+	var yi []int
+	for _, v := range vi {
+		if v%2 == 0 {
+			yi = append(yi, v)
+		}
+	}
+	return f(yi...)
+}
