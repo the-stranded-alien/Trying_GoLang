57b6d88b78d926821fce680de191013d75765298
Sahil Gupta <54513905+the-stranded-alien@users.noreply.github.com>
2022-11-17 20:20:26 +0530

    pointers intro

---
+++ pointers.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	a := 42
+	fmt.Println(a)
+	fmt.Println(&a) // & gives you the address
+
+	fmt.Printf("%T\n", a)
+	fmt.Printf("%T\n", &a)
+
+	b := &a
+	fmt.Println(b)
+	fmt.Println(*b)
+
+	fmt.Println(*&a) // * gives you the value stored at an address when you have the address
+
+	*b = 43
+	fmt.Println(a)
+}
